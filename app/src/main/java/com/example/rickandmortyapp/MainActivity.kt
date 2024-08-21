a96064b2e01c5bff3cd4b5d99d25c2efc272d31f
Hernan Miranda <mirandahernan2002@gmail.com>
2024-08-21 16:41:05 -0300

    inicio del proyecto

---
+++ app/src/main/java/com/example/rickandmortyapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.rickandmortyapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.example.rickandmortyapp.ui.theme.RickAndMortyAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RickAndMortyAppTheme {
+
+            }
+        }
+    }
+}
